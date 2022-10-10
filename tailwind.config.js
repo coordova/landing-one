7a6b4493c71335852dd1cba92d7397639a975631
coordova <grcordova@gmail.com>
2022-10-10 18:44:30 -0400

    landing-one - basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/**/*.js"],
+  theme: {
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "720px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "960px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}

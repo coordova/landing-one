4c5d41104786a0f3f9cf053d6073e0c970fc0910
coordova <grcordova@gmail.com>
2022-10-12 23:08:33 -0400

    top menu v1 - assets assets repository

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./*.html", "./assets/**/*.js"],
   theme: {
@@ -22,7 +23,23 @@ module.exports = {
       center: true,
       padding: "16px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        first: "#4338CA",
+
+        black: "#212b36",
+        dark: "#090E34",
+        "dark-700": "#090e34b3",
+        primary: "#3056D3",
+        secondary: "#13C296",
+        "body-color": "#637381",
+        warning: "#FBBF24",
+      },
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }

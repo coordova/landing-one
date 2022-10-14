cf53eae6707fb87c5df1fd38396b7d62bf9fdc66
coordova <grcordova@gmail.com>
2022-10-13 20:11:33 -0400

    partner section - grid version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,9 @@
 module.exports = {
   content: ["./*.html", "./assets/**/*.js"],
   theme: {
-    screens: {
-      sm: "540px",
-      // => @media (min-width: 576px) { ... }
-
-      md: "720px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "960px",
-      // => @media (min-width: 992px) { ... }
-
-      xl: "1140px",
-      // => @media (min-width: 1200px) { ... }
-
-      "2xl": "1320px",
-      // => @media (min-width: 1400px) { ... }
-    },
     container: {
       center: true,
-      padding: "16px",
+      padding: "3rem", /* 16px */
     },
     extend: {
       colors: {

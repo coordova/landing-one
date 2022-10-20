2f5fc4372b1447b670f39d707f2379d77812a116
coordova <grcordova@gmail.com>
2022-10-20 19:28:02 -0400

    CTA & Footer v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,31 @@
 module.exports = {
   content: ["./*.html", "./assets/**/*.js"],
   theme: {
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "720px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "960px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
     container: {
       center: true,
-      padding: "16px", /* 16px */
+      /*padding: "16px",  16px */
+      padding: "2rem",
     },
     extend: {
       colors: {
+        bgDarkFooter: "#192132",
+
         first: "#4338CA",
 
         black: "#212b36",
